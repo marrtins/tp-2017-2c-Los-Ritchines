6bf88637b0d44114cd795998b75f9a0dd1313fe4
carogon <carolina.malen.gonzalez@gmail.com>
2017-10-09 18:22:45 -0300

    gero rompio las bolas

--- dataNode/src/dataNode.c
+++ dataNode/src/dataNode.c
@@ -10,20 +10,19 @@ int main(int argc, char* argv[]) {
 	Theader *head = malloc(sizeof(Theader));
 	head->tipo_de_proceso = DATANODE;
 	head->tipo_de_mensaje = INFO_NODO;
-
 	FILE * archivo = fopen("/home/utnso/tp-2017-2c-Los-Ritchines/dataNode/data1.bin", "rb+");
-	fd = fileno(archivo);
-
-	if ((archivoMapeado = mmap(NULL, BLOQUE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED,	fd, 0)) == MAP_FAILED) {
-					logAndExit("Error al hacer mmap");
-			}
-	fclose(archivo);
-	close(fd);
 
 	if(argc!=2){
 			printf("Error en la cantidad de parametros\n");
 			return EXIT_FAILURE;
 		}
+
+	fd = fileno(archivo);
+	if ((archivoMapeado = mmap(NULL, BLOQUE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED,	fd, 0)) == MAP_FAILED) {
+					logAndExit("Error al hacer mmap");
+			}
+	fclose(archivo);
+	close(fd);
 
 	logger = log_create("dataNode.log", "dataNode", false, LOG_LEVEL_INFO);
 	dataNode = obtenerConfiguracionDN(argv[1]);
