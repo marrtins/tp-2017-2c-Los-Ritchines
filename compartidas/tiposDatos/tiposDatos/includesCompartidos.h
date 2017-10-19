563db6c9c0f8b5b1c0a61b841f11881b2bd88a1d
Torrico Juan Manuel <jmanueltorrico_0593@hotmail.com>
2017-10-19 15:00:11 -0300

    Update includesCompartidos.h

---
+++ compartidas/tiposDatos/tiposDatos/includesCompartidos.h
@@ -0,0 +1,19 @@
+#ifndef COMPARTIDAS_INCLUDESCOMPARTIDOS_H_
+#define COMPARTIDAS_INCLUDESCOMPARTIDOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/collections/list.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/log.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <math.h>
+
+
+#endif
