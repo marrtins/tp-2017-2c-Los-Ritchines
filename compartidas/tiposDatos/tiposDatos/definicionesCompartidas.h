f310c169c38e141723e15da37d8fe1e796555414
Torrico Juan Manuel <jmanueltorrico_0593@hotmail.com>
2017-10-19 15:03:51 -0300

    Update definicionesCompartidas.h

---
+++ compartidas/tiposDatos/tiposDatos/definicionesCompartidas.h
@@ -0,0 +1,17 @@
+#ifndef COMPARTIDAS_DEFINICIONESCOMPARTIDAS_H_
+#define COMPARTIDAS_DEFINICIONESCOMPARTIDAS_H_
+
+#include "includesCompartidos.h"
+
+#define MAXIMO(A,B) ((A) > (B) ? (A) : (B))
+#define MAXIMA_LONGITUD_PUERTO 6
+#define MAXIMA_LONGITUD_IP 16
+#define BACKLOG 20
+#define HEAD_SIZE 8
+#define BLOQUE_SIZE 1024*1024
+#define TAMANIO_EXTENSION_ARCHIVO 10
+#define NOMBRE_NODO 7
+
+
+
+#endif
