5bde3fd13fceb0558e6317df7050710b63794612
gerosamore <cortigeronimo@gmail.com>
2017-11-03 12:14:07 -0300

    cat funcionando

--- fileSystem/src/lib/funciones/tablaDirectorios.c
+++ fileSystem/src/lib/funciones/tablaDirectorios.c
@@ -204,7 +204,7 @@ void leerArchivoComoTextoPlano(char * rutaLocal){
 	Tarchivo * archivo = malloc(sizeof(Tarchivo));
 
 	levantarTablaArchivo(archivo,rutaLocal);
-
+	mkdir("/home/utnso/tp-2017-2c-Los-Ritchines/fileSystem/src/metadata/tmp/", 0777);
 	string_append(&rutaTmp, "/home/utnso/tp-2017-2c-Los-Ritchines/fileSystem/src/metadata/tmp/");
 	string_append(&rutaTmp, nombreArchivoConExtension);
 	levantarArchivo(archivo,rutaTmp);
@@ -213,7 +213,7 @@ void leerArchivoComoTextoPlano(char * rutaLocal){
 	}else{
 		mostrarBinario(rutaTmp);
 	}
-
+	remove(rutaTmp);
 	liberarTablaDeArchivo(archivo);
 	free(nombreArchivoConExtension);
 	free(extension);
