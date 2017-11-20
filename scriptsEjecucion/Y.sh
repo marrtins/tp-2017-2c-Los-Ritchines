63b368e81c2c33493d3906d6573440ce89589a61
marrtins <martinsilva.1995@gmail.com>
2017-11-20 11:19:11 -0300

    nuevos scripts

--- scriptsEjecucion/Y.sh
+++ scriptsEjecucion/Y.sh
@@ -1,3 +1,3 @@
 cd tp-2017-2c-Los-Ritchines/yama/src/ ;
-gcc -g3 -Wall yama.c lib/funcionesYM.c lib/almacenamiento.c lib/planificacion.c lib/reduccionGlobal.c lib/reduccionLocal.c lib/transformacion.c  -lfuncionesCompartidas -lcommons -lreadline -lpthread -o ~/y.out ;
+gcc -g3 -Wall yama.c lib/funcionesYM.c lib/almacenamiento.c lib/reduccionGlobal.c lib/reduccionLocal.c lib/planificacion.c lib/transformacion.c  -lfuncionesCompartidas -lcommons -lreadline -lpthread -o ~/y.out ;
  cd
